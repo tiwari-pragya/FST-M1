d138bb61e43aa8b4fe05810c9afe3a25288321c8
tiwari-pragya <141907510+tiwari-pragya@users.noreply.github.com>
2023-09-27 21:36:37 +0530

    Add files via upload

--- Python/Activities/Activity10.py
+++ Python/Activities/Activity10.py
@@ -1,9 +1,9 @@
 # create tuple
-numbers = (10, 20, 33, 46, 55)
+numbers = (25, 40, 33, 22, 51, 88, 20)
 print("Number list is ", numbers)
 
 # check if divisible by 5
 print("Numbers divisible by 5:")
 for num in numbers:
-    if num % 5==0:
-        print (num)
+    if num % 5 == 0:
+        print(num)
